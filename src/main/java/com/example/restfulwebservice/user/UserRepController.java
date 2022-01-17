f1a744c40fdead0f89f004a5afe8f83f6db2bdec
rpcopopqpq@gmail.com <rpcopopqpq@gmail.com>
2022-01-17 17:36:38 +0900

    add create user api using jpa

--- src/main/java/com/example/restfulwebservice/user/UserRepController.java
+++ src/main/java/com/example/restfulwebservice/user/UserRepController.java
@@ -1,11 +1,21 @@
 package com.example.restfulwebservice.user;
 
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/jpa")
 @AllArgsConstructor
@@ -17,5 +27,34 @@ public class UserRepController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable final Integer id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new NotFoundUserException(String.format("ID[%s] Not Found", id));
+        }
+        //HATEOAS
+        EntityModel<User> model = EntityModel.of(user.get());
+        model.add(
+                WebMvcLinkBuilder.linkTo(
+                                WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers())
+                        .withRel("all-users"));
+
+        return model;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody final User user) {
+        User savedUser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity
+                .created(location)
+                .build();
     }
 }
